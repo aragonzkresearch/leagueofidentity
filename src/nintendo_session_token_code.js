dcec1f71694639fec8ed9fb24ae357b27854c502
vincenzoiovino <vinciovino@gmail.com>
2024-01-18 21:07:03 +0100

    nintendo provider

---
+++ src/nintendo_session_token_code.js
@@ -0,0 +1,16 @@
+function nintendo_session_token_code(nst_code) {
+    const params = {};
+    const codeVerifier = nst_code.split('@')[0];
+    const nst_url = nst_code.split('@')[1];
+    nst_url.split('#')[1]
+        .split('&')
+        .forEach(str => {
+            const splitStr = str.split('=');
+            params[splitStr[0]] = splitStr[1];
+        });
+    return  codeVerifier + "@" + params.session_token_code;
+}
+
+module.exports = {
+    nintendo_session_token_code
+}
